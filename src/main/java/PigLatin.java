fcabb1606590da61a65c7b5de278a14a1f410d83
AvicianPenguin <aleksandervolkov2007@gmail.com>
2025-01-22 09:03:32 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -45,56 +45,22 @@ public class PigLatin {
  	return -1;
     }
 
-public void setup(){
-  String [] lines = {"beast", "dough", "happy", "question", "star", "three", "eagle", "try"};
-  for (int i = 0 ; i < lines.length; i++){
-    System.out.println(pigLatin(lines[i]));
-   }
-}
-public void draw()
-{
-        //not used
-}
-public int findFirstVowel(String word){
-if(word.length()>0){
-  for(int i = 0; i<word.length(); i++){
-    if(word.substring(i+0,i+1).equals("a"))
-      return i;
-    else if(word.substring(i+0,i+1).equals("e"))
-      return i;
-    else if(word.substring(i+0,i+1).equals("i"))
-      return i;
-    else if(word.substring(i+0,i+1).equals("o"))
-      return i;
-    else if(word.substring(i+0,i+1).equals("u"))
-      return i;
-  }
- }
- return -1;
-}
 
-public String pigLatin(String word){
-if(word.length()>0){
-//postcondition: returns the pig latin equivalent of sWord
-
-    if(findFirstVowel(word) == -1)
-    
-      return word + "ay";
-    
-    if(findFirstVowel(word) == 0)
-    
-      return word + "way";
-    
-    if(word.substring(0,2).equals("qu"))
- 
-      return word.substring(2,word.length()) + "quay";
-    for(int i = 0; i<word.length(); i++){
-      if(findFirstVowel(word) == i){
-        return(word.substring(i,word.length())+word.substring(0,i)+"ay");     
-      }
-    }   
-  }
-  return "ERROR!";
- }
-}
+    public String pigLatin(String word){
+	if(word.length()>0){
+	//postcondition: returns the pig latin equivalent of sWord
+    		if(findFirstVowel(word) == -1)
+      			return word + "ay";
+    		if(findFirstVowel(word) == 0)
+      			return word + "way";
+    		if(word.substring(0,2).equals("qu"))
+      			return word.substring(2,word.length()) + "quay";
+    		for(int i = 0; i<word.length(); i++){
+     			if(findFirstVowel(word) == i){
+        			return(word.substring(i,word.length())+word.substring(0,i)+"ay");
+			}
+		}   
+	}
+  	return "ERROR!";
+    }
 	
